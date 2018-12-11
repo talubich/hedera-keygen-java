dc56a7f58ed35fb5da219ccaf826fa60dd325906
Nathan Klick <nathan@swirlds.com>
2018-12-11 17:33:41 -0600

    Added additional BC dependency

--- src/main/java/com/hedera/sdk/keygen/KeyStoreGen.java
+++ src/main/java/com/hedera/sdk/keygen/KeyStoreGen.java
@@ -1,11 +1,20 @@
 package com.hedera.sdk.keygen;
 
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x500.X500NameBuilder;
+import org.bouncycastle.cert.X509v1CertificateBuilder;
+import org.bouncycastle.jcajce.provider.asymmetric.X509;
+import org.bouncycastle.x509.X509V1CertificateGenerator;
+import org.bouncycastle.x509.extension.X509ExtensionUtil;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.security.KeyStore;
 import java.security.KeyStore.PasswordProtection;
 import java.security.KeyStore.PrivateKeyEntry;
 
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.cert.Certificate;
 
 public class KeyStoreGen {
@@ -27,6 +36,7 @@ public class KeyStoreGen {
 		final Certificate[] certificates = new Certificate[]{};
 		final PrivateKeyEntry privateKeyEntry = new PrivateKeyEntry(keyPair.getPrivateKey(), certificates);
 		final PasswordProtection passwordProtection = new PasswordProtection(passphrase, DEFAULT_PROTECTION_ALGORITHM, null);
+
 		try (FileOutputStream fos = new FileOutputStream(filename)){
 			final KeyStore keyStore = KeyStore.getInstance(DEFAULT_KEY_STORE_TYPE);
 			keyStore.setEntry(PRIVATE_KEY_ALIAS, privateKeyEntry, passwordProtection);
@@ -54,4 +64,10 @@ public class KeyStoreGen {
 			throw new RuntimeException(exception);
 		}
 	}
+
+//	public static Certificate createCertificate(PublicKey publicKey, PrivateKey privateKey) {
+//		X500Name dn = new X500Name("CN=" + PRIVATE_KEY_ALIAS);
+//		X509v1CertificateBuilder builder = new X509v1CertificateBuilder();
+//
+//	}
 }
